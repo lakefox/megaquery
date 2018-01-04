2b943a4f3dd2e2188a7b7e0de01d1bc245c948e9
LakeFox <mason@lakefox.net>
2018-01-04 17:49:40 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -10,14 +10,11 @@ try {
   fs.mkdirSync(base);
 }
 
-// /database/q=pp5 l1l
-// /database/key=pp5 l1l&value={}
+// /database/?q=pp5 l1l
+// /database/?key=pp5 l1l&value={}
 app.get("/database/*", (req,res) => {
   if (req.query.q) {
     var q = req.query.q;
-    for (var i = 0; i < q.length; i++) {
-      res.end("{error: 'query not passible'}");
-    }
     var query = base+"/"+q.split("").join("/")+"/data.json";
     fs.readFile(query, (err, data) => {
       if (err) {
@@ -29,29 +26,30 @@ app.get("/database/*", (req,res) => {
   } else if (req.query.key && req.query.value) {
     var key = req.query.key;
     var value = req.query.value;
-    if (!req.query.overwrite) {
-      try {
-        fs.statSync(base+"/"+key.split("").join("/"));
-      } catch (e) {
-        res.end("{error: 'file exists, overwrite with &overwrite=true'}")
-
-      }
+    try {
+      fs.statSync(base+"/"+key.split("").join("/"))
+    } catch (e) {
+      var err = true;
     }
-    for (var i = 1; i < key.length+1; i++) {
-      var path = base+"/"+key.slice(0,i).split("").join("/");
-      try {
-        fs.statSync(path);
-      } catch (e) {
-        fs.mkdirSync(path);
+    if (err && req.query.overwrite != "true") {
+      res.end("{error: 'file exists, overwrite with &overwrite=true'}")
+    } else {
+      for (var i = 1; i < key.length+1; i++) {
+        var path = base+"/"+key.slice(0,i).split("").join("/");
+        try {
+          fs.statSync(path);
+        } catch (e) {
+          fs.mkdirSync(path);
+        }
       }
+      fs.writeFile(path+"/data.json",value, (err) => {
+        if (err) {
+          res.end("{err: "+err+"}");
+        } else {
+          res.end("{sucess: true}");
+        }
+      });
     }
-    fs.writeFile(path+"/data.json",value, (err) => {
-      if (err) {
-        res.end("{err: "+err+"}");
-      } else {
-        res.end("{sucess: true}");
-      }
-    });
   }
 });
 
